78399700a224338fa96acb39d5764a9a8a1b7474
Kiryl_Bakhur <bahur201554@gmail.com>
2022-08-30 16:10:32 +0300

    New

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Console.WriteLine("Введите число: ");
+// Console.Write("");
+// int x = Convert.ToInt32(Console.ReadLine());
+// string Day(int x)
+// {
+//     if (x == 1) return "No";
+//     if (x == 2) return "No";
+//     if (x == 3) return "No";
+//     if (x == 4) return "No";
+//     if (x == 5) return "No";
+//     if (x == 6) return "Yes";
+//     if (x == 7) return "Yes";
+//     else return ("Введено неккоректное число");
+// }
+
+// Console.WriteLine("Введите число: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// string xnum = Convert.ToString(x);
+// if (xnum.Length > 2)
+// {
+//    Console.WriteLine("Вторая цифра:" + xnum[2]);
+// }
+// else
+// {
+//     Console.WriteLine ("Неправильно введенно значение");
+// }
+
+// Console.WriteLine("Введите число: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// string xnum = Convert.ToString(x);
+// if (xnum.Length > 1)
+// {
+//    Console.WriteLine("Вторая цифра:" + xnum[1]);
+// }
+// else
+// {
+//     Console.WriteLine ("Неправильно введенно значение");
+// }
